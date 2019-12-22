a78780a0976d0ef731e19b0a149e37dfb2b999a2
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2019-12-22 18:55:52 +0900

    Update

--- ComInfo.cs
+++ ComInfo.cs
@@ -4,8 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// 共通情報のロジック
+/// </summary>
 public class ComInfo
 {
+    /// <summary>
+    /// Pixelの列挙
+    /// </summary>
     public enum Pixel
     {
         B = 0,
